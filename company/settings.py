ad418451f017fa79532e9f34cd640e8ec74f6ae6
Jopher <johnchristopher.seletaria@gmail.com>
2025-11-01 12:38:54 +0800

    01-11-2025 - adding cd and render yml

--- company/settings.py
+++ company/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = "django-insecure-e(zuzy@5u$mq^^az=e7=zrn^h%mp#z97p0y($#bflxe4as20_1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ["your-app-name.onrender.com"]
 
 # for creating a webserver locally
 ALLOWED_HOSTS = [
