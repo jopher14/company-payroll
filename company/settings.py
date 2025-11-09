6bcaddf067a9253270cd1505e47d514ddce326ed
Jopher <johnchristopher.seletaria@gmail.com>
2025-11-09 23:42:40 +0800

    09-11-2025 - adding url.onrender in settings

--- company/settings.py
+++ company/settings.py
@@ -91,6 +91,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://192.168.100.154:3000",
+    "https://company-payroll.onrender.com",
 ]
 
 ROOT_URLCONF = "company.urls"
