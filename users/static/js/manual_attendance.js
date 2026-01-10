f26ec1d6b6504c3a600dcba3deb54cfc978131d5
Jopher <johnchristopher.seletaria@gmail.com>
2026-01-10 20:09:19 +0800

    10-01-2026 - separate the css and js in manual request html

---
+++ users/static/js/manual_attendance.js
@@ -0,0 +1,13 @@
+flatpickr("#time_in", {
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true,
+});
+
+flatpickr("#time_out", {
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true,
+});
